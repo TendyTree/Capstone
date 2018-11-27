bbc77e29d075ce11a4da6a0b2d64b62a66aad9e4
xxaxun1t <hockeyfan2@live.com>
2018-11-27 00:42:18 -0500

    Cleaning code
    Added some exception

--- Reader.py
+++ Reader.py
@@ -2,14 +2,26 @@
 # on ip 192.168.0.10 from the OBD2 reader for analysisself.
 # Andrew Brashich
 import socket
-
-LOGSOCKET = socket.socket()
+x = True
+LOGSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LOGGING = open("logging.txt","w+")
 OBD2_IP = "192.168.0.10"
 OBD2_PORT = 3500
+LOGSOCKET.settimeout(10)
+print("loop")
 
-LOGSOCKET.connect((OBD2_IP, OBD2_PORT))
-while True:
+try:
+    LOGSOCKET.connect((OBD2_IP, OBD2_PORT))
+except socket.timeout:
+    x = False
+    print("Could not connect to the OBD2, please make sure it is plugged in.")
+except socket.error:
+    x = False
+    print("Error trying to connect")
+
+LOGSOCKET.settimeout(None)
+print("Printing data)
+while (x):
     data = LOGSOCKET.recv(32)
     if not data:
         break
@@ -17,3 +29,4 @@ while True:
     LOGGING.write(data)
 
 LOGGING.close()
+print("Closed")
