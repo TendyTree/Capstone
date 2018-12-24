30c25abd1afaef36a172e57247afb8e7599c6075
Andrew <hockeyfan2@live.com>
2018-12-23 23:11:55 -0500

    Final commit for Client and Server Connections

--- Reader.py
+++ Reader.py
@@ -2,31 +2,33 @@
 # on ip 192.168.0.10 from the OBD2 reader for analysisself.
 # Andrew Brashich
 import socket
-x = True
+
+#Set up Variables for the connection
 LOGSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LOGGING = open("logging.txt","w+")
 OBD2_IP = "192.168.0.10"
 OBD2_PORT = 3500
-LOGSOCKET.settimeout(10)
-print("loop")
 
+#sets the timeout for the connection to the OBD2 reader to 10 seconds.
+#If not connected in amount of time it will throw an error to you
+LOGSOCKET.settimeout(10)
 try:
     LOGSOCKET.connect((OBD2_IP, OBD2_PORT))
+
+    #Waits to recieve the data from the OBD2 reader and then stores it
+    #in the logging file.
+    while True:
+        data = LOGSOCKET.recv(32)
+        if not data:
+            break
+        print(data)
+        LOGGING.write(data.decode('utf-8'))
 except socket.timeout:
-    x = False
     print("Could not connect to the OBD2, please make sure it is plugged in.")
 except socket.error:
-    x = False
     print("Error trying to connect")
 
-LOGSOCKET.settimeout(None)
-print("Printing data)
-while (x):
-    data = LOGSOCKET.recv(32)
-    if not data:
-        break
-    print(data)
-    LOGGING.write(data.decode('utf-8'))
-
+#Closes the program
+LOGSOCKET.close()
 LOGGING.close()
 print("Closed")
--- Server.py
+++ Server.py
@@ -4,14 +4,23 @@
 # Andrew Brashich
 import socket
 from time import sleep
+
+#Variables for local host
 HOST_IP = "127.0.0.1"
 HOST_PORT = 3500
+
+#Opens the save logging data for use
 with open('logging.txt','r') as LOGGING:
+
+    #Initialize the socket for the server and set it to listening
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as LOGSERVER:
         LOGSERVER.bind((HOST_IP, HOST_PORT))
         LOGSERVER.listen(1)
         print("listening")
         conn, addr = LOGSERVER.accept()
+
+        #With the connection, send all data from file with a 5 milisecond delayself.
+        #After it closes the connection
         with conn:
             print('Connected by', addr)
             for line in LOGGING:
