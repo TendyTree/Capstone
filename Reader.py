38a672b498b8d947f27787ca3282f81c45eff464
tendy <hockeyfan2@live.com>
2019-01-13 21:05:38 -0500

    Reader should be done testing tomorrow.
    Server still needs some work

--- Reader.py
+++ Reader.py
@@ -12,23 +12,28 @@ OBD2_PORT = 3500
 
 def send_recv(_Send_command):
     _data = ''
-    _decode_data=''
+    _decode_data = _Send_command + '\n'
     _Send_command += '\r'
     LOGSOCKET.sendall(_Send_command)
     print()
-        while True:
-            data = LOGSOCKET.recv(128)
-            print(data)
-            if not data
-
-
-    return _decode_data
+    while True:
+        _data = LOGSOCKET.recv(64)
+        print(data)
+        if _data.endswith(b'\r') or _data>128 or _data == '>':
+            _decode_data = _decode_data + a.rstrip().decode('utf8') + "\n"
+            if _data == '>':
+                break
+    print(_decode_data)
+    return _decode_data #contains _Send_command + \n + _data + \n + _data + \n...
 
 def _InitOBD():
-    with open("Init.txt","w+") as _Init_file
+    with open("Init.txt","w+") as _Init_file:
         _Init_commands = ("ATZ","ATD0","ATSP0","ATE0","ATH1","ATST64","ATS0","ATAT1","0100")
         for _commands in _Init_commands:
-            _Init_file.write(_commands + '\n' + send_recv(_commands) + '\n')
+            _recv_data = send_recv(_commands)
+            _Init_file.write(_recv_data)
+            if 'NODATA' in _recv_data or 'CAMERROR' in _recv_data:
+                return False
         print("Done with INIT")
     return True
 
@@ -36,40 +41,32 @@ def _InitOBD():
 
 def _get_data():
     _data = ''
-    with open("logging1.txt","w+") as _Logging
+    with open("logging1.txt","w+") as _Logging:
         _Pids = ('010C','010D','0105','019A','0145','0146','015C','015E','01A4','01A6','ATRV')
-        while True
+        while True:
             for pids in _Pids:
-                _data = send_recv(pids)
-                _Logging.write(pids + '\n' + _data + '\n')
-                if _data == 'CAMERROR' or _data = 'NODATA'
+                _recv_data = send_recv(pids)
+                _Logging.write(_recv_data)
+                if 'NODATA' in _recv_data or 'CAMERROR' in _recv_data:
                     break
-
-
-
-    while True:
-        data = LOGSOCKET.recv(32)
-        if not data:
-            break
-        print(data)
-        LOGGING.write(data.decode('utf-8'))
-
-
 
 
 def main():
     #sets the timeout for the connection to the OBD2 reader to 10 seconds.
     #If not connected in amount of time it will throw an error to you
     LOGSOCKET.settimeout(10)
+    print('try')
     try:
         LOGSOCKET.connect((OBD2_IP, OBD2_PORT))
+        LOGSOCKET.settimeout(None)
+        print('hi')
+        if _InitOBD() == True:
+                _get_data()
     except socket.timeout:
             print("Could not connect to the OBD2, please make sure it is plugged in.")
     except socket.error:
             print("Error trying to connect")
-    LOGSOCKET.settimeout(None)
-    if _InitOBD == True:
-        _get_data()
+
 
     #Closes the program
     LOGSOCKET.close()
--- Server.py
+++ Server.py
@@ -8,20 +8,60 @@ from time import sleep
 #Variables for local host
 HOST_IP = "127.0.0.1"
 HOST_PORT = 3500
-#All files that need to be created
-#RPM
-#SPEED
-#COOLANT
-#VOLTAGE
-#ERROR
-#ENGINELOAD
-#MAF
-#THROTTLE
-#AIRTEMP
-#OILTEMP
-#FUELRATE
-#GEAR
-#ODOMETER
+_Pids = [
+['010C\r'],
+['010D\r'],
+['0105\r'],
+['019A\r'],
+['0145\r'],
+['0146\r'],
+['015C\r'],
+['015E\r'],
+['01A4\r'],
+['01A6\r'],
+['ATRV\r']
+]
+_Start_AT = [
+['ATZ\r'],
+['ATD0\r'],
+['ATSP0\r'],
+['ATE0\r'],
+['ATH1\r'],
+['ATST64\r'],
+['ATS0\r'],
+['ATAT1\r'],
+['0100\r']
+]
+
+def _Init_files():
+    _list_iter = 0
+    _list_bool = False
+    with open("Init.txt","w+") as _Init_file:
+        for _line in _Init_file:
+            _line = _line.rstrip() + '\r'
+            for i, _Init_AT in enumerate(_Start_AT):
+                if _Init_AT == _line:
+                    _list_iter = i
+                    _list_bool = true
+                    break
+            if  not _list_bool:
+                _Start_AT[_list_iter].append(_line)
+            else:
+                _list_bool = false
+    _list_iter = 0
+    _list_bool = False
+    with open("logging1.txt","w+") as _Logging:
+        for _line in _Logging:
+            _line = _line.rstrip() + '\r'
+            for i, _Init_PIDS in enumerate(_Pids):
+                if _Init_PIDS == _line:
+                    _list_iter = i
+                    _list_bool = true
+                    break
+            if  not _list_bool:
+                _Pids[_list_iter].append(_line)
+            else:
+                _list_bool = false
 
 #Opens the save logging data for use
 with open('logging.txt','r') as LOGGING:
@@ -35,11 +75,11 @@ with open('logging.txt','r') as LOGGING:
 
         #With the connection, send all data from file with a 5 milisecond delayself.
         #After it closes the connection
-		
-		#recieves DATA and then parse what needed 
+
+		#recieves DATA and then parse what needed
 		#switch case then sends out the data
-		
-		
+
+
         with conn:
             print('Connected by', addr)
             for line in LOGGING:
